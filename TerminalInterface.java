5bd1873c4e4a19f207a66db8a390d2e2245cf57d
brianpereboom <52140839+brianpereboom@users.noreply.github.com>
2022-12-01 06:09:40 -0800

    Add files via upload

--- TerminalInterface.java
+++ TerminalInterface.java
@@ -5,12 +5,12 @@ public class TerminalInterface {
             switch (args[0]) {
                 case "RGB": {
                     double[] hsv = ColorConverter.toHSV(Double.valueOf(args[1]), Double.valueOf(args[2]), Double.valueOf(args[3]));
-                    System.out.println("HSV " + String.format("%.1f", hsv[0]) + " " + String.format("%.3f", hsv[1]) + " " + String.format("%.3f", hsv[2]));
+                    System.out.printf("HSV %.1f %.3f %.3f\n", hsv[0], hsv[1], hsv[2]);
                     break;
                 }
                 case "HSV": {
                     double[] rgb = ColorConverter.toRGB(Double.valueOf(args[1]), Double.valueOf(args[2]), Double.valueOf(args[3]));
-                    System.out.println("RGB " + String.format("%.0f", rgb[0]) + " " + String.format("%.0f", rgb[1]) + " " + String.format("%.0f", rgb[2]));
+                    System.out.printf("RGB %.0f %.0f %.0f\n", rgb[0], rgb[1], rgb[2]);
                     break;
                 }
                 default:
